c1bc205ea77344543f9981e3dd2388da68a16f79
thiago custodio <thiago custodio>
2016-10-20 08:29:32 -0200

    initial commit

---
+++ RefactoringProject.Test/ShortNameAnalyzerTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestHelper;
+using RefactoringProject;
+
+namespace RefactoringProject.Test
+{
+    [TestClass]
+    public class ShortNameAnalyzerTest : CodeFixVerifier
+    {
+        [TestMethod]
+        public void ShortNameDeclaration()
+        {
+            const string test = @"
+                    using System;
+                    namespace ConsoleApplication1
+                    {
+                        class Foo
+                        {
+                            void Bar()
+                            {
+                                string x = ""Me"";
+                            }
+                        }
+                    }";
+
+            var expected = new DiagnosticResult
+            {
+                Id = "ShortNameAnalyzer",
+                Message = "Try to use a better name",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 9, 33)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new ShortNameAnalyzer();
+        }
+    }
+}
---
+++ RefactoringProject/ShortNameAnalyzer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace RefactoringProject
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ShortNameAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "ShortNameAnalyzer";
+
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.ShortNameTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.ShortNameMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.ShortNameDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Naming";
+
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeSymbol, SyntaxKind.LocalDeclarationStatement);
+        }
+
+        private static void AnalyzeSymbol(SyntaxNodeAnalysisContext context)
+        {
+            var localDeclaration = (LocalDeclarationStatementSyntax)context.Node;
+
+            var variableDeclaration = context.Node.ChildNodes().OfType<VariableDeclarationSyntax>().FirstOrDefault();
+
+            foreach (var variable in variableDeclaration.Variables)
+            {
+                if (variable.Identifier.Text.Length <= 3)
+                {
+                    var diagnostic = Diagnostic.Create(Rule, variableDeclaration.Type.GetLocation());
+                    context.ReportDiagnostic(diagnostic);
+                }
+            }
+        }
+    }
+}
